df9724a41641d2c74b5193d31f7bce238a86661a
Alexis Montes <montes.alexis25@gmail.com>
2021-08-06 01:40:27 -0400

    created initial user mongoose model

---
+++ models/user.js
@@ -0,0 +1,26 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
