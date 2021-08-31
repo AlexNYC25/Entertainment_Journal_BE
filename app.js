dda37ba9db55480143571a7351d5e9f9861acfc7
Alexis Montes <montes.alexis25@gmail.com>
2021-08-30 21:44:32 -0400

    added dotenv package and connected to mongodb

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+let mongoose = require('mongoose');
+require('dotenv').config({path: './.env'})
+
+
+try {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+} catch (error) {
+  handleError(error);
+}
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let newUsers = require('./routes/newUser');
 
 var app = express();
 
@@ -21,6 +32,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/new', newUsers);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
