dc1b3129ae7e8a72ac981defb5f028e4f49718c6
Alexis Montes <montes.alexis25@gmail.com>
2021-10-25 01:33:07 -0400

    finished newUser registration route

--- routes/newUser.js
+++ routes/newUser.js
@@ -39,40 +39,64 @@ let userToReturn = (user) => {
     }
 }
 
-
-router.post('/newUser', (req, res) => {
-    let newUserEmail = req.body.email;
-    let newUserPassword = req.body.password;
+/*
+    Route to create a new user
+    POST /newUser/register
+    Body: {
+        newUserEmail: <string>,
+        newUserPassword: <string>
+    }
+*/
+router.post('/register', (req, res) => {
+    let newUserEmail = req.body.email.toString();
+    let newUserPassword = req.body.password.toString();
+    let newUserFirstName = req.body.firstName.toString();
+    let newUserLastName = req.body.lastName.toString();
 
     // check if email is valid and if password is valid
     if(verifyEmail(newUserEmail) && verifyPassword(newUserPassword)) {
 
-        // generate salt for password hashing
-        bcrypt.genSalt(10, (err, salt) => {
-            // hash password
-            bcrypt.hash(newUserPassword, salt, (err, hash) => {
-                if(err) {
-                    // error hashing password
-                    return res.status(500).send({status: 'error', message: 'Error trying to encrypt password', error: err});
-                }
-                else {
-                    // create new user using User model
-                    User.create({
-                        email: newUserEmail,
-                        password: hash,
-                        firstName: req.body.firstName.toString(),
-                        lastName: req.body.lastName.toString()
-                    }, (err, user) => {
+        // check if user already exists
+        User.findOne({email: newUserEmail}, (err, user) => {
+            if(err) {
+                res.status(500).send({ error: 'Internal server error' });
+                return
+            }
+            if(user) {
+                res.status(400).send({ error: 'User already exists' });
+                return
+            }
+            else{
+                // generate salt for password hashing
+                bcrypt.genSalt(10, (err, salt) => {
+                    // hash password
+                    bcrypt.hash(newUserPassword, salt, (err, hash) => {
                         if(err) {
-                            // error creating user
-                            return res.status(500).send({status: 'error', message: 'Error creating new user', error: err});
-                        } else {
-                            return res.status(200).send({status: 'success', message: 'New user created', user: userToReturn(user)});
+                            // error hashing password
+                            return res.status(500).send({status: 'error', message: 'Error trying to encrypt password', error: err});
+                        }
+                        else {
+                            // create new user using User model
+                            User.create({
+                                email: newUserEmail,
+                                password: hash,
+                                firstName: newUserFirstName,
+                                lastName: newUserLastName
+                            }, (err, user) => {
+                                if(err) {
+                                    // error creating user
+                                    return res.status(500).send({status: 'error', message: 'Error creating new user', error: err});
+                                } else {
+                                    return res.status(200).send({status: 'success', message: 'New user created', user: userToReturn(user)});
+                                }
+                            });
                         }
                     });
-                }
-            });
+                });
+            }
         });
+
+        
     }
     else {
         return res.status(400).send({status: 'error', message: 'Invalid email or password'});
